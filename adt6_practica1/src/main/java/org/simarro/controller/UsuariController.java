92b20ec7929a8af50636fcf2c30c765b764dcaeb
santacata <rubensantacatalinapellicer@gmail.com>
2026-03-09 12:26:54 +0100

    Eliminar el signin2, estoy viendo los apuntes y el signin 1 es necesario aunque no se use

--- adt6_practica1/src/main/java/org/simarro/controller/UsuariController.java
+++ adt6_practica1/src/main/java/org/simarro/controller/UsuariController.java
@@ -42,32 +42,6 @@ public class UsuariController {
         return new ResponseEntity<>(lista, HttpStatus.OK);
     }
 
-    @GetMapping("/signIn")
-    @Operation(summary = "Obtiene el usuario por signin")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Existe el usuario",
-                    content = @Content(schema = @Schema(implementation = Usuari.class))),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "No se puede obtener el usuario",
-                    content = @Content(schema = @Schema(implementation=ResponseEntity.class)))
-    })
-    public ResponseEntity<Usuari> listarId(@RequestParam(value = "nia") Integer nia,
-                                                 @RequestParam(value = "contrasena") String contrasena) {
-        List<Usuari> lista = service.listar();
-        for(Usuari u :lista){
-            if (u.getNia()==nia&&u.getPassword().equalsIgnoreCase(contrasena)){
-                // Código 200 OK para select
-                return new ResponseEntity<>(u, HttpStatus.OK);
-            }
-        }
-
-        return new ResponseEntity<>(new Usuari(), HttpStatus.NO_CONTENT);
-
-    }
-    
 
 
 
